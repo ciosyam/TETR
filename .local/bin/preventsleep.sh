8ec8bde54c082d9e688f750fa06d2d0c9f9f4952
ciosyam <e1121211047@student.untan.ac.id>
2024-03-14 07:00:24 +0800

    14-maret

--- .local/bin/preventsleep.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Prevent the system from suspending and screen blanking when the lid is closed
-while true; do
-    sleep 5
-    grep -q closed /proc/acpi/button/lid/*/state && xset s off -dpms
-done
