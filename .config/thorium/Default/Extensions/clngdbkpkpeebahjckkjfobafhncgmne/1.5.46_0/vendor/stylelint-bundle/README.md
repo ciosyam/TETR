3ebd92d86887a8771cbd655a753c48aa7983d412
ciosyam <e1121211047@student.untan.ac.id>
2024-03-14 23:53:55 +0800

    14-maret

--- .config/thorium/Default/Extensions/clngdbkpkpeebahjckkjfobafhncgmne/1.5.46_0/vendor/stylelint-bundle/README.md
+++
@@ -1,4 +0,0 @@
-## stylelint-bundle v15.10.2
-
-Files copied from NPM (node_modules):
-* stylelint-bundle.min.js: dist/* (removed sourceMappingURL)
---
+++ .local/bin/preventsleep.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Prevent the system from suspending and screen blanking when the lid is closed
+while true; do
+    sleep 5
+    grep -q closed /proc/acpi/button/lid/*/state && xset s off -dpms
+done
